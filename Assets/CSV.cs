26488634f83cb3bb9d77075b55fe16437d677e11
f11m4at4 <f11m4at4@gmail.com>
2022-09-30 16:26:58 +0900

    .

--- Assets/CSV.cs
+++ Assets/CSV.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Text;
 using UnityEngine;
@@ -16,11 +17,63 @@ public class CSV : MonoBehaviour
         }
     }
 
+    public List<T> Parsing<T>(string fileName) where T : new()
+    {
+        List<T> list = new List<T>();
+
+        string path = Application.streamingAssetsPath + "/" + fileName + ".csv";
+
+        //파일 열자
+        byte[] byteData = File.ReadAllBytes(path);
+        string stringData = Encoding.GetEncoding("euc-kr").GetString(byteData);
+        //string stringData = File.ReadAllText(path);
+
+        //엔터(\n) 기준으로 한줄씩 자르기
+        string[] lines = stringData.Split("\n");
+        //\r 을 제거
+        for (int i = 0; i < lines.Length; i++)
+        {
+            lines[i] = lines[i].Replace("\r", "");
+        }
+
+        //변수 나누기
+        string[] variable = lines[0].Split(",");
+
+        //값 나누기
+        for (int i = 1; i < lines.Length; i++)
+        {
+            //만약에 값이 없다면(길이가 0이라면)
+            if (lines[i].Length == 0) continue;
+
+            string[] value = lines[i].Split(",");
+
+            //정보 만들기
+            T data = new T();
+
+            for(int j = 0; j < variable.Length; j++)
+            {
+                //variable[0] = "name", variable[1] = "phone", variable[2] = "age"
+                //해당 이름으로 되어있는 변수의 정보를 얻어오자
+                System.Reflection.FieldInfo fieldInfo = typeof(T).GetField(variable[j]);
+                //int.parse, float.parse... 무엇을 할지 결정해놓다.
+                TypeConverter typeConverter = TypeDescriptor.GetConverter(fieldInfo.FieldType);
+                //값을 셋팅
+                //data.age = int.Parse(value[2]);
+                fieldInfo.SetValue(data, typeConverter.ConvertFrom(value[j]));
+            }
+
+            //정보 추가
+            list.Add(data);
+        }
+
+        return list;
+    }
+
     public List<UserInfoA> Parsing(string fileName)
     {
         List<UserInfoA> list = new List<UserInfoA>();
 
-        string path = Application.dataPath + "/" + fileName + ".csv";
+        string path = Application.streamingAssetsPath + "/" + fileName + ".csv";
 
         //파일 열자
         byte [] byteData = File.ReadAllBytes(path);
--- Assets/DataManager.cs
+++ Assets/DataManager.cs
@@ -11,12 +11,23 @@ public class UserInfoA
     public int age;
 }
 
+[System.Serializable]
+public class UserInfoB
+{
+    public string name;
+    public string phone;
+    public string email;
+    public int age;
+}
+
 public class DataManager : MonoBehaviour
 {
     public static DataManager instance;
 
     //A반 학생들 정보 담을 변수
     public List<UserInfoA> userInfoA;
+    //B반 학생들 정보 담을 변수
+    public List<UserInfoB> userInfoB;
 
     private void Awake()
     {
@@ -34,11 +45,20 @@ public class DataManager : MonoBehaviour
 
     void Start()
     {
-        userInfoA = CSV.instance.Parsing("Table_Contact_A");
+        userInfoA = CSV.instance.Parsing<UserInfoA>("Table_Contact_A");
+        for(int i = 0; i < userInfoA.Count; i++)
+        {
+            print(userInfoA[i].name);
+        }
+        //userInfoB = CSV.instance.Parsing<UserInfoB>("Table_Contact_B");            
     }
+
 
     void Update()
     {
-        
     }
+
+    
 }
+
+
